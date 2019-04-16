e3481e6a2123fc1e696954c5a4b85bde3f7ef8e6
vitordl <vitortactic@gmail.com>
2019-04-16 14:03:52 -0300

    script adicionado

---
+++ scripts/meuScript.js
@@ -0,0 +1,9 @@
+function mudaCor(cor){
+	var elemento =  document.getElementById("mensagem");
+	elemento.style.color = cor;
+}
+
+function mudaFundo(cor){
+	var elemento2 = document.getElementById("insight");
+	elemento2.style.background = cor;
+}
